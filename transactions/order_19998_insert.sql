889199267867870abbd82178cc9e5421fbd95850
Paul Brabban <paul.brabban@gmail.com>
2024-06-15 19:47:59 +0100

    Create order_19998_insert.sql

---
+++ transactions/order_19998_insert.sql
@@ -0,0 +1,4 @@
+-- this is how comments are written in SQL
+BEGIN; -- order_id 19998: insert alone in transaction
+INSERT INTO orders VALUES(19998,'VINET', 5, '1996-07-04', '1996-08-01', '1996-07-16', 3, 32.3800011, 'Vins et alcools Chevalier', '59 rue de l''Abbaye', 'Reims', NULL, '51100', 'France');
+COMMIT;
