2f6e35b8b2655ca326d40d7b42866ebfadb4d028
Paul Brabban <paul.brabban@gmail.com>
2024-06-15 20:27:15 +0100

    Create order_20001_rollback.sql

---
+++ transactions/order_20001_rollback.sql
@@ -0,0 +1,5 @@
+-- order_id 20001, insert and rollback
+
+BEGIN;
+INSERT INTO orders VALUES(20001,'VINET', 5, '1996-07-04', '1996-08-01', '1996-07-16', 3, 32.3800011, 'Vins et alcools Chevalier', '59 rue de l''Abbaye', 'Reims', NULL, '51100', 'France');
+ROLLBACK;
