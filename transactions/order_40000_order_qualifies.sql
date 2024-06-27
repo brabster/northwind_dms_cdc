644769dfa80053621b35e6fe0714c4a3ef3940af
Paul Brabban <paul.brabban@gmail.com>
2024-06-27 21:37:20 +0100

    Create order_40000_order_qualifies.sql

---
+++ transactions/order_40000_order_qualifies.sql
@@ -0,0 +1,15 @@
+-- simulates queuing of statements in a transaction as user makes edits
+BEGIN;
+-- only one day between order and required - does not qualify for promotion
+INSERT INTO orders VALUES(40000,'VINET', 5, '1996-07-04', '1996-07-05', NULL, 3, 32.3800011, 'Vins et alcools Chevalier', '59 rue de l''Abbaye', 'Reims', NULL, '51100', 'France');
+COMMIT;
+
+BEGIN;
+-- user updates the required date - now qualifies for promotion if shipped
+UPDATE orders SET required_date = '1996-08-25' WHERE order_id = 40000;
+COMMIT;
+
+BEGIN;
+-- user sets the shipped_date - order qualifies for promotion
+UPDATE orders SET shipped_date = '1996-08-01' WHERE order_id = 40000;
+COMMIT;
