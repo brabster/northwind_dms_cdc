2d199af2e9050f3a01519eee8cf1531ed64499de
Paul Brabban <paul.brabban@gmail.com>
2024-06-28 08:28:55 +0100

    Create order_30102_order_qualification_changes.sql

---
+++ transactions/order_30102_order_qualification_changes.sql
@@ -0,0 +1,20 @@
+-- order has multiple qualification states after shipped_date is set
+BEGIN;
+-- only one day between order and required - does not qualify for promotion
+INSERT INTO orders VALUES(30102,'VINET', 5, '1996-07-04', '1996-07-05', NULL, 3, 32.3800011, 'Vins et alcools Chevalier', '59 rue de l''Abbaye', 'Reims', NULL, '51100', 'France');
+COMMIT;
+
+BEGIN;
+-- user sets the shipped_date - order does not qualify for promotion
+UPDATE orders SET shipped_date = '1996-08-01' WHERE order_id = 30102;
+COMMIT;
+
+BEGIN;
+-- user updates the required date - now qualifies for promotion
+UPDATE orders SET required_date = '1996-08-25' WHERE order_id = 30102;
+COMMIT;
+
+BEGIN;
+-- user updates the shipped_date - order does not qualify for promotion
+UPDATE orders SET employee_id = 4 WHERE order_id = 30102;
+COMMIT;
